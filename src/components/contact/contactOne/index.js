e9a4c37d9c5a343c68662c0f7d5228f223353675
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-06-14 09:32:49 +0530

    contact us form done

--- src/components/contact/contactOne/index.js
+++ src/components/contact/contactOne/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "../../Header1";
 import Footer from "../../Footer1";
 import Title from "./Title";
-import Form from "./Form";
+import ContactForm from "./Form";
 
 const ContactOne = () => {
 	return (
 		<>
 			<Header />
 			<Title />
-			<Form />
+			<ContactForm />
 			<Footer />
 		</>
 	);
