6c342b7122c68e7a5ef95778d712959e02a31040
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2021-08-03 13:23:27 +0530

    order success page change

--- src/components/home/OrderCreate/OrderSuccess.js
+++ src/components/home/OrderCreate/OrderSuccess.js
@@ -1,26 +1,47 @@
-import React, { useLayoutEffect } from 'react'
-import Header1 from '../../Header1'
-import Footer1 from '../../Footer1'
+import React, { useLayoutEffect } from "react";
+import Header1 from "../../Header1";
+import Footer1 from "../../Footer1";
+import { Row, Col } from "antd";
+import "./OrderSuccess.css";
 const OrderSuccess = () => {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
-    useLayoutEffect(() => {
-        window.scrollTo(0, 0)
-      });
+  return (
+    <main>
+      <Header1 />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="order-container">
+        <div className="printer-top"></div>
 
-    return (
-        <main>
-        <Header1 />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <div>
-        <h1>Thank you for the purchase.. We'll get back to you soon..</h1>
+        <div className="paper-container">
+          <div className="printer-bottom"></div>
+
+          <div className="paper">
+            <div className="main-contents">
+              <div className="success-icon">&#10004;</div>
+              <div className="success-title">Order Success</div>
+              <div className="success-description">
+                We received your purchase request; we'll be in touch shortly!
+              </div>
+              {/* <div className="order-details">
+                <div className="order-number-label">Order Number</div>
+                <div className="order-number">123456789</div>
+              </div> */}
+              <div className="order-footer">Thank you!</div>
+            </div>
+            {/* <div className="jagged-edge"></div> */}
+          </div>
         </div>
-        <Footer1 />
-        </main>
-    )
-}
+      </div>
+      {/* <Footer1 /> */}
+    </main>
+  );
+};
 
-export default OrderSuccess
+export default OrderSuccess;
